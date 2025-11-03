5e5e9c770b4103880950bf82577e01548737acb8
CyberYeti <yitmengchin@gmail.com>
2025-11-03 14:46:37 -0800

    Working Brute Force solver. May miss some edge cases in restricted views such as multiple ways to insert a pair into a slot

--- SpeedCube Libraries Test/BruteSolver.py
+++ SpeedCube Libraries Test/BruteSolver.py
@@ -2,31 +2,72 @@ from CubeMoveSim import rotate_cube
 
 #states are expected to be in string format:
 # UP, LEFT, FRONT, RIGHT, BACK, DOWN.
-startState = ""
-targetState = ""
-startIntermediates = {}
-endIntermediates = {}
 possibleMoves = [x+y for x in ["U","D","L","R","F","B"] for y in ["","2","'"]]
 
-def generateIntermediates(state: str, depth: int) -> dict[str, list[str]]:
-    intermediates = {state: []}
+def generateIntermediates(state: str, depth: int) -> dict[str, str]:
+    intermediates = {}
+    intermediates[state] = ""
+    currentLevel = [state]
+    nextLevel = []
 
-    def backtrack(currentState: str, currentDepth: int, path: list[str]):
-        if currentDepth == depth:
-            return
-        for move in possibleMoves:
-            newState = rotate_cube(currentState, move)
-            if newState not in intermediates:
-                intermediates[newState] = path + [move]
-                backtrack(newState, currentDepth + 1, path + [move])
+    for _ in range(depth):
+        for cubeState in currentLevel:
+            for move in possibleMoves:
+                newState = rotate_cube(cubeState, move)
+                if newState not in intermediates:
+                    intermediates[newState] = intermediates[cubeState] + move + " "
+                    nextLevel.append(newState)
+        currentLevel = nextLevel
+        nextLevel = []
 
-    backtrack(state, 0, [])
     return intermediates
 
-def findSolution(start: str, target: str, maxDepth: int = 6) -> list[str]:
+def invertMove(move: str) -> str:
+    if move.endswith("'"):
+        return move[:-1]
+    elif move.endswith("2"):
+        return move
+    else:
+        return move + "'"
+
+def findSolution(start: str, target: str, maxDepth: int = 6) -> set[str]:
+    startState = start.replace(" ", "")
+    targetState = target.replace(" ", "")
+    
+    if len(startState) != 54 or len(targetState) != 54:
+        raise ValueError("Invalid cube state length. Expected 54 characters.")
+
     frontSteps = maxDepth // 2
     backSteps = maxDepth - frontSteps
 
-    return []
+    frontIntermediates = generateIntermediates(startState, frontSteps)
+    backIntermediates = generateIntermediates(targetState, backSteps)
+
+    solutions = set()
+    for state in frontIntermediates:
+        if state in backIntermediates:
+            frontMoves = frontIntermediates[state].strip().split(" ")
+            backMoves = backIntermediates[state].strip().split(" ")
+            backMoves = [invertMove(m) for m in reversed(backMoves)]
+            solutions.add(" ".join(frontMoves + backMoves))
+
+    return solutions
 
     # Generate all possible intermediate states from the start state
+
+OYBLOCK = "xxxxxxxxx xxxoooooo xxxgxxgxx xxxxxxxxx xxxxxbxxb yxxyxxyxx".upper()
+cur = "xxOxxxxGx xxxxOxxxx xOxxxYxxO xxBOxxGBx Yxxxxxxxx xxYxxOxxx".upper()
+
+if __name__ == "__main__":
+    import magiccube
+    from richFormatting import rich_print_colored
+    startState = "WWWWWWWWWOOOOOOOOOGGGGGGGGGRRRRRRRRRBBBBBBBBBYYYYYYYYY"
+    cube = magiccube.Cube(3, OYBLOCK.replace("X","W"))
+    print("Target State:")
+    rich_print_colored(str(cube))
+
+    cube = magiccube.Cube(3, cur.replace("X","W"))
+    print("Current State:")
+    rich_print_colored(str(cube))
+
+    print(findSolution(cur, OYBLOCK, maxDepth=12))
