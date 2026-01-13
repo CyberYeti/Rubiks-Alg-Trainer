2118db4918c0b3ae76a4ec902d3930fc3862fc10
CyberYeti <yitmengchin@gmail.com>
2026-01-12 18:54:14 -0800

    abstracted general cube functions into CubeUtils

---
+++ main/CubeUtils.py
@@ -0,0 +1,65 @@
+import json
+import kociemba
+
+def kociemba_solve(cube_state, end_state = None):
+
+    if end_state is None:
+        end_state = "WWWWWWWWWOOOOOOOOOGGGGGGGGGRRRRRRRRRBBBBBBBBBYYYYYYYYY"
+    return kociemba.solve(_cvt_to_kociemba(cube_state), _cvt_to_kociemba(end_state))
+
+def _cvt_to_kociemba(cube_str):
+    """
+    Convert a 54-character cube string from ULFRBD face order
+    to URFDLB (Kociemba) face order.
+    """
+    if len(cube_str) != 54:
+        raise ValueError("Cube string must be exactly 54 characters")
+
+    colormap = {}
+    colormap[cube_str[4]] = "U"  # Center of U face
+    colormap[cube_str[13]] = "L" # Center of L face
+    colormap[cube_str[22]] = "F" # Center of F face
+    colormap[cube_str[31]] = "R" # Center of R face
+    colormap[cube_str[40]] = "B" # Center of B face
+    colormap[cube_str[49]] = "D" # Center of D face
+    
+    kociemba_str = "".join(colormap[char] for char in cube_str)
+
+    faces = [kociemba_str[i:i+9] for i in range(0, 54, 9)]
+
+    # ULFRBD → URFDLB
+    order = [
+        0,  # U
+        3,  # R
+        2,  # F
+        5,  # D
+        1,  # L
+        4   # B
+    ]
+
+    return "".join(faces[i] for i in order)
+
+def invert_moves(sequence):
+    moves = sequence
+
+    if type(sequence) == str:
+        moves = sequence.split()
+
+    inverted = []
+    for move in reversed(moves):
+        if move.endswith("'"):
+            inverted.append(move[:-1])
+        elif move.endswith("2"):
+            inverted.append(move)
+        else:
+            inverted.append(move + "'")
+
+    if type(sequence) == str:
+        inverted = " ".join(inverted)
+
+    return inverted
+
+def getAlgs(path):
+    with open(path, 'r') as file:
+        data = json.load(file)
+    return [alg['algorithm'] for alg in data['algs']]
--- main/ScrambleCreator.py
+++ main/ScrambleCreator.py
@@ -1,65 +1,6 @@
-import json
 import random
-import magiccube
-import kociemba
 import GenerateMoveSequences
-
-def cvt_to_kociemba(cube_str):
-    """
-    Convert a 54-character cube string from ULFRBD face order
-    to URFDLB (Kociemba) face order.
-    """
-    if len(cube_str) != 54:
-        raise ValueError("Cube string must be exactly 54 characters")
-
-    colormap = {}
-    colormap[cube_str[4]] = "U"  # Center of U face
-    colormap[cube_str[13]] = "L" # Center of L face
-    colormap[cube_str[22]] = "F" # Center of F face
-    colormap[cube_str[31]] = "R" # Center of R face
-    colormap[cube_str[40]] = "B" # Center of B face
-    colormap[cube_str[49]] = "D" # Center of D face
-    
-    kociemba_str = "".join(colormap[char] for char in cube_str)
-
-    faces = [kociemba_str[i:i+9] for i in range(0, 54, 9)]
-
-    # ULFRBD → URFDLB
-    order = [
-        0,  # U
-        3,  # R
-        2,  # F
-        5,  # D
-        1,  # L
-        4   # B
-    ]
-
-    return "".join(faces[i] for i in order)
-
-def invert_moves(sequence):
-    moves = sequence
-
-    if type(sequence) == str:
-        moves = sequence.split()
-
-    inverted = []
-    for move in reversed(moves):
-        if move.endswith("'"):
-            inverted.append(move[:-1])
-        elif move.endswith("2"):
-            inverted.append(move)
-        else:
-            inverted.append(move + "'")
-
-    if type(sequence) == str:
-        inverted = " ".join(inverted)
-
-    return inverted
-
-def getAlgs(path):
-    with open(path, 'r') as file:
-        data = json.load(file)
-    return [alg['algorithm'] for alg in data['algs']]
+from CubeUtils import invert_moves, getAlgs, kociemba_solve
 
 ollAlgs = getAlgs('Algs/CrossOLL.json')
 pllAlgs = getAlgs('Algs/PLL.json')
@@ -72,7 +13,7 @@ def SpecificScramble():
     BASESTATE = "WWWWWWWWWOOOOOOOOOGGGGGGGGGRRRRRRRRRBBBBBBBBBYYYYYYYYY"
     scrambledState = GenerateMoveSequences.rotate_cube(BASESTATE, inverseScramble)
 
-    kociembaScramble = invert_moves(kociemba.solve(cvt_to_kociemba(scrambledState)))
+    kociembaScramble = invert_moves(kociemba_solve(scrambledState))
     return kociembaScramble, oll, pll
 
 if __name__ == "__main__":
